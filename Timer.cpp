0da46001514f53f3e6a3837b70fbaa0b47dccce3
wba6 <bradenaey1@gmail.com>
2025-01-21 19:41:01 -0500

    Add files

---
+++ Timer.cpp
@@ -0,0 +1,7 @@
+/*
+ * A source file for a timer to keep track of the time passed
+ *
+ * Author wba6
+ */
+
+#include "Timer.h"
---
+++ Timer.h
@@ -0,0 +1,18 @@
+/*
+ * A header file for a timer to keep track of the time passed
+ *
+ * Author wba6
+ */
+
+#ifndef PERFORMANCE_ANALYER_TIMER_H
+#define PERFORMANCE_ANALYER_TIMER_H
+
+class Timer {
+public:
+
+private:
+
+};
+
+
+#endif //PERFORMANCE_ANALYER_TIMER_H
