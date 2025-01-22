dbd31dbfaa0b68a9dfcc06a6e0ae8ab47b172770
wba6 <bradenaey1@gmail.com>
2025-01-21 20:04:59 -0500

    Add vars

--- Timer.h
+++ Timer.h
@@ -7,6 +7,9 @@
 #ifndef PERFORMANCE_ANALYER_TIMER_H
 #define PERFORMANCE_ANALYER_TIMER_H
 
+#include <memory>
+#include <chrono>
+
 /*
  * A timer class to keep track of the time passed between two events
  */
@@ -46,6 +49,14 @@ public:
     void stop();
 private:
 
+    // start time of the timer
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+
+    // end time of the timer
+    std::chrono::time_point<std::chrono::high_resolution_clock> end_time;
+
+    // time span between start and end time
+    std::chrono::duration<double> time_span;
 };
 
 
