357f2adbca4965f7c146011ed01abde4252d5188
wba6 <bradenaey1@gmail.com>
2025-01-22 12:42:58 -0500

    Add function signature to report

--- Profiler.h
+++ Profiler.h
@@ -15,7 +15,21 @@
 #include "Timer.h"
 
 
+
+
 #ifdef PROFILING_ENABLED
+
+/*
+ * A macro to get the function signature
+ */
+#if defined(_MSC_VER)
+#define FUNCTION_SIGNATURE __FUNCSIG__
+#elif defined(__GNUC__) || defined(__clang__)
+#define FUNCTION_SIGNATURE __PRETTY_FUNCTION__
+#else
+#define FUNCTION_SIGNATURE __func__
+#endif
+
 /*
  * A macro to create a profile scope
  *
@@ -26,7 +40,7 @@
 /*
  * A macro to create a profile function
  */
-#define PROFILE_FUNCTION() PROFILE_SCOPE(__FUNCTION__)
+#define PROFILE_FUNCTION() PROFILE_SCOPE(FUNCTION_SIGNATURE)
 #else
 #define PROFILE_SCOPE(name)
 #define PROFILE_FUNCTION()
